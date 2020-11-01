30d74f72b9d70c2fe130baed945622b3148e081e
Weerawat Saothong <Weerawatsaotong.gmail.com>
2020-11-02 00:49:33 +0700

    initial commit added MyNavbar component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MyNavbar, {  } from "./components/my-navbar/mynavbar.component";
+  
+import './App.css';
+
+const App = () => {
+  return (<div> 
+    <MyNavbar/>
+    My Portfolio
+    </div>);  
+};
+
+export default App;
