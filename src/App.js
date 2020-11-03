b9b72047058441ab2e381ff17570fd03cb9fa1d0
Weerawat Saothong <Weerawatsaotong.gmail.com>
2020-11-03 16:43:23 +0700

    Added about assets

--- src/App.js
+++ src/App.js
@@ -2,14 +2,34 @@ import React from "react";
 import MyNavbar, {  } from "./components/my-navbar/mynavbar.component";
 import  MyCarousal from "./components/my-carousal/my-carousal.component";
 import TitleMessage from "./components/title-message/title-message.component";
+import About from "./pages/about/about.component";
+import Fade from "react-reveal/Fade";
+import { Parallax} from 'react-parallax';
+import Container from "react-bootstrap/Container";
+import bgImg from './assets/img/parallex/background.webp';
+
 import './App.css';
 
 const App = () => {
-  return (<div> 
+  return (<div  className="App" style={{ position: "relative" }}> 
     <MyNavbar/>
     <MyCarousal />
     <TitleMessage/>
-    </div>);  
+    <div>
+    <Parallax 
+          blur={{ min:-30, max: 30 }}
+          bgImage={bgImg}  bgImageAlt="img error" strength={-200}>
+          <div>
+            <Container className="container-box rounded">
+              <Fade duration={1000}>
+                <About />
+              </Fade>
+            </Container>
+          </div>
+        </Parallax>
+      </div>
+    </div>
+    );  
 };
 
 export default App;
---
+++ src/pages/about/about.component.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from 'react-bootstrap/Image';
+import Profile from "../../assets/img/profile/P.png";
+import Button from "react-bootstrap/Button";
+import "./about.style.css";
+const About = () => {
+    return (
+      <div id="about">
+        <div className="about">
+          <h1 className="pt-3 text-center font-details pb-3">ABOUT ME</h1>
+          <Container>
+            <Row className="pt-3 pb-5 align-items-center">
+              <Col xs={12} md={6}>
+                <Row className="justify-content-center mb-2 mr-2 ">
+                  <Image className="profile justify-content-end" alt="profile" src={Profile} thumbnail fluid />
+                </Row>
+              </Col>
+              <Col xs={12} md={6}>
+                <Row className=" align-items-start p-2 my-details rounded">
+                  Hi! I am <strong>&nbsp;Weerawat Saothong</strong>
+                  <br />I'm from Thailand, I live in Kalasin Province, I'm 23 years old.<br />
+                  In the year 2020 I completed my bachelor's degree 
+                  <br /> 
+                  at Rajabhat Mahasarakham University in Computer Science.
+                  <br /> 
+                  I love learning about new technologies and I am committed to bringing
+                  My computer programming knowledge
+                  <br />
+                  In programming and web development design to meet the needs of most users.
+                  <br /> 
+
+                  <Col className="d-flex justify-content-center flex-wrap">
+                  <div>
+                    <a href="https://drive.google.com/file/d/1Jnmb6Ww1wXGf1J9Hw62jJeOgybecLeZe/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-danger">
+                        My Resume
+                      </Button>
+                    </a>
+                  </div>
+                  <div>
+                    <a href="https://github.com/Weerawat-Saothong" target="_blank" rel="noopener noreferrer">
+                      <Button className="m-2" variant="outline-dark">
+                        GitHub
+                      </Button>
+                    </a>
+                  </div>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default About;
